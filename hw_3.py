46393191018866bfe45a787e36231bef550505d3
sonzza <sonzza.life@gmail.com>
2020-03-28 18:04:26 +0300

    hw 3 part 2

--- hw_3.py
+++ hw_3.py
@@ -1,6 +1,18 @@
 ﻿import requests
-import json
 import bs4
+from pymongo import MongoClient
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (
+    Column,
+    Integer,
+    ForeignKey,
+    String,
+    Table
+)
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
 
 headers = {'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/80.0.3987.122 Safari/537.36'}
@@ -30,7 +42,7 @@ def get_page(url):
 def get_post_data(post_url: str) -> dict:
     template_data = {'title': '',
                      'url': '',
-                     'coment_count': '',
+                     'comment_count': '',
                      'public_time': '',
                      'writer': {'name': '',
                                 'url': ''},
@@ -55,11 +67,88 @@ def get_post_data(post_url: str) -> dict:
 
 
 if __name__ == '__main__':
+    # Mongo_time
+    #     client_mongo = MongoClient('mongodb://127.0.0.1:27017/')
+    #     db = client_mongo['habr_top_weekly']
+    #
+    #     for soap in get_page(URL):
+    #         posts = get_post_url(soap)
+    #         data = [get_post_data(url) for url in posts]
+    #         db['posts'].insert_many(data)
+    #         # for post in data:
+    #         #     with open('{name}.json'.format(name=post['title'].replace('/', '_')), 'w') as file:
+    #         #         file.write(json.dumps(post))
+
+    # SQL_time
+
+    Base = declarative_base()
+
+    association_table = Table('association', Base.metadata,
+                              Column('post_id', Integer, ForeignKey('comment_writer.id')),
+                              Column('comment_writer_id', Integer, ForeignKey('post.id')))
+
+    class Comment_writer(Base):
+        __tablename__ = 'comment_writer'
+        id = Column(Integer, primary_key=True, autoincrement=True)
+        name = Column(String, unique=False, nullable=False)
+        url = Column(String, unique=True, nullable=False)
+        # post = relationship('Post', back_populates='comment_writer')
+
+        def __init__(self, name, url, post):
+            self.name = name
+            self.url = url
+            self.post = post
+
+
+    class Writer(Base):
+        __tablename__ = 'writer'
+        id = Column(Integer, primary_key=True, autoincrement=True)
+        name = Column(String, unique=False, nullable=False)
+        url = Column(String, unique=True, nullable=False)
+
+        def __init__(self, name, url):
+            self.name = name
+            self.url = url
+
+
+    class Post(Base):
+        __tablename__ = 'post'
+        id = Column(Integer, primary_key=True, autoincrement=True)
+        title = Column(String, unique=False, nullable=False)
+        url = Column(String, unique=True, nullable=False)
+        comment_count = Column(String, unique=False, nullable=True)
+        writer_id = Column(Integer, ForeignKey('writer.id'))
+        writer = relationship('Writer', backref='post')
+        comment_writer = relationship('Comment_writer',  secondary=association_table)
+
+        def __init__(self, title, url, comment_count, writer):
+            self.title = title
+            self.url = url
+            self.comment_count = comment_count
+            self.writer = writer
+
+
+    engine = create_engine('sqlite:///habr_blog.db')
+    Base.metadata.create_all(engine)
+
+    session_db = sessionmaker(bind=engine)
+
+    session = session_db()
+
     for soap in get_page(URL):
         posts = get_post_url(soap)
-        data = [get_post_data(url) for url in posts]
-        for post in data:
-            with open('{name}.json'.format(name=post['title'].replace('/', '_')), 'w') as file:
-                file.write(json.dumps(post))
-
+        for url in posts:
+            data = get_post_data(url)
+            sql_writer = Writer(data['writer']['name'], data['writer']['url'])
+            sql_post = Post(data['title'], data['url'], data['comment_count'], sql_writer.id)
+            sql_comment_writers = []
+            for item in data['comment_writers']:
+                sql_comment_writer = Comment_writer(item,
+                                                    data['comment_writers'][item], sql_post.id)
+                sql_comment_writers.append(sql_comment_writer)
+            session.add_all(sql_post)
+            session.add(sql_writer)
+            session.commit()
+            session.add(sql_comment_writer)
+            session.commit()
 
